4042e03f680daf21fa669fae96036c55fbc3297a
R2D2 <R2D2@DESKTOP-ITDF4MN>
2017-04-23 01:17:04 +0300

    Добавьте файлы проекта.

---
+++ boxcom/Controllers/CompareController.cs
@@ -0,0 +1,28 @@
+﻿using boxcom.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace boxcom.Controllers
+{
+    public class CompareController : Controller
+    {
+        // GET: Compare
+        public ActionResult Select()
+        {
+            return View();
+        }
+
+        public ActionResult Result()
+        {
+            return View(new List<Boxer>() { new Boxer().getBoxer(Request.Form["b1"]), new Boxer().getBoxer(Request.Form["b2"]) });
+        }
+
+        public ActionResult Catalog()
+        {
+            return View();
+        }
+    }
+}
